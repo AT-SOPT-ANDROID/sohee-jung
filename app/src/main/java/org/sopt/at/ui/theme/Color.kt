289cd9255ef496f9209290a83a26490c274f2266
sohee <soheeaubrey@ewha.ac.kr>
2025-04-11 13:08:57 +0900

    featute/SignIn - 비밀번호 숨김, 회원가입 이동

--- app/src/main/java/org/sopt/at/ui/theme/Color.kt
+++ app/src/main/java/org/sopt/at/ui/theme/Color.kt
@@ -1,6 +1,7 @@
 package org.sopt.at.ui.theme
 
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.GraphicsContext
 
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
@@ -9,3 +10,9 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val GrayButton = Color(0xFF404040)
+val GrayEditText = Color(0xFF262626)
+val GrayEdit = Color(0XFF262626)
+val GrayHintText = Color(0xFF767676)
+val GrayText = Color(0xFFb5b5b5)
